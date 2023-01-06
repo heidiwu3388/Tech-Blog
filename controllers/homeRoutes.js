36eb28c33b7debf18ab3e9d84e09440233fa366e
heidiwu3388 <heidiwu3388@yahoo.com>
2023-01-06 12:28:47 -0800

    create home route '/'

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { posts }); 
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+module.exports = router;
