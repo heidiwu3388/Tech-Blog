6aff41a9571221cb8a3aae0337099cae6a768bc4
heidiwu3388 <heidiwu3388@yahoo.com>
2023-01-06 13:18:52 -0800

    bring in helpers for handlebars

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 require('dotenv').config();
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine
-const hbs = exphbs.create();
+const hbs = exphbs.create({helpers});
 
 const sess = {
   secret: process.env.SESSION_SECRET,
